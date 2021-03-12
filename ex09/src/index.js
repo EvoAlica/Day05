9489b97a5d4b414cba7d32e804f7b92d3d6a2725
Anela Alic <ane@Tariks-MacBook-Air.local>
2021-03-13 00:03:49 +0100

    Added Day05

---
+++ ex09/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Doe";
+    var firstLeetterOfLastName =  lastName[0];
+    // Only change code above this line
+    return firstLeetterOfLastName;
+
+}
+
+console.log(main());
+module.exports = main;
