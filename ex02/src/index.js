845f8934699a7e799c821579660e82d0ef0575d1
Anela Alic <ane@Tariks-MacBook-Air.local>
2021-03-12 16:04:49 +0100

    Added index.js in src folder

---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+    // Only cahnge code below this line
+    var myLink = '"<a href=\"http:www.example.com\" target=\"_blank\">Link</a>"';
+    // Only change code below this line
+    return myLink
+}
+console.log(main());
+module.exports = main;
